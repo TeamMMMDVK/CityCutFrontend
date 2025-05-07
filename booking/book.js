53c9a8ac43d6ae31f9123a208b4d88d8ee4ffe3e
MetteMunch <meha0005@stud.kea.dk>
2025-05-07 15:54:33 +0200

    lavet hurtig indledende opsætning med basic html og css

--- booking/book.js
+++ booking/book.js
@@ -1,4 +1,4 @@
 export default () => `
-    <h1>Book</h1>
+    <h1>Book en tid</h1>
     <p>Consectetur in vitae totam nulla reprehenderit est earum debitis quam laboriosam.</p>
 `;
---
+++ contact.js
@@ -0,0 +1,8 @@
+export default function() {
+    return `
+        <section class="content-section">
+            <h2>Kontakt</h2>
+            <p>Kontaktinformation og formular.</p>
+        </section>
+    `;
+}
