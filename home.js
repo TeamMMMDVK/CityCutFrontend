0afee5f596bd94bc7147c2b7b14a63996ee7fec6
MetteMunch <meha0005@stud.kea.dk>
2025-05-16 12:14:54 +0200

    CITY-138 gemt billede

--- home.js
+++ home.js
@@ -1,4 +1,4 @@
 export default () => `
-    <h1>Home</h1>
-    <p>Consectetur in vitae totam nulla reprehenderit est earum debitis quam laboriosam.</p>
+    <h1>Home</h1> 
+    <img src="images/imageHome.jpg" alt="Home Image, ">
 `;
