837dac81bd3a28ba76efc2f5bf2380bb81871718
DanielJensenKEA <daje0003@stud.kea.dk>
2025-05-17 12:25:39 +0200

    Render available timeslot selection UI. Made getavailabletimeslot export. Added CSS. The rendertimeslotselecton needs to post a DTO to backend to create a booking.

--- booking/book.js
+++ booking/book.js
@@ -1,4 +1,40 @@
-export default () => `
+import {getAvailableTimeslots} from "./timeslots.js";
+export default () => {
+    setTimeout(renderTimeslotSelection, 0)
+    return `
     <h1>Book en tid</h1>
     <p>Consectetur in vitae totam nulla reprehenderit est earum debitis quam laboriosam.</p>
-`;
+    <div id="bookingContainer"></div>
+`};
+async function renderTimeslotSelection() {
+    const timeslotArr = await getAvailableTimeslots()
+    const bookingContainer = document.getElementById("bookingContainer")
+    let selectedTimeslots = JSON.parse(localStorage.getItem("selectedTimeSlots")) || []
+
+    timeslotArr.forEach(t => {
+        let timeslotDiv = document.createElement("div")
+        timeslotDiv.id = t.id
+        timeslotDiv.innerHTML = t.time
+        timeslotDiv.classList.add("timeslot")
+        bookingContainer.appendChild(timeslotDiv)
+
+        if (selectedTimeslots.some(slot => slot.id === t.id)) {
+            timeslotDiv.classList.add("selected")
+        }
+
+        timeslotDiv.addEventListener('click', function() {
+            const index = selectedTimeslots.findIndex(slot => slot.id === t.id)
+
+            if (index > -1 ) {
+                selectedTimeslots.splice(index, 1)
+                timeslotDiv.classList.remove("selected")
+            } else {
+                selectedTimeslots.push(t)
+                timeslotDiv.classList.add("selected")
+            }
+            localStorage.setItem("selectedTimeslot", JSON.stringify(selectedTimeslots))
+        })
+    })
+
+
+}
